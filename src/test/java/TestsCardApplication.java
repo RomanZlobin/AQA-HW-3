0404ffeb7adeb01cf5abd6c77722fa55d08e47bf
RomanZlobin <rvzlobin@gmail.com>
2022-09-20 19:01:09 +0700

    web driver manager

--- src/test/java/TestsCardApplication.java
+++ src/test/java/TestsCardApplication.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,7 @@ public class TestsCardApplication {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
+        WebDriverManager.chromedriver().setup();;
     }
 
     @BeforeEach
